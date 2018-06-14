37f5c85267823b391492aa5f698c14247ee7a56e
criosmartinez <criosmartinez@berkeley.edu>
2018-06-14 13:05:23 -0700

    week1 completed assignments

---
+++ week1/ch05/bigger_better_favorite_number.rb
@@ -0,0 +1,4 @@
+puts 'What is your favorite number?'
+favoriteNumber = gets.chomp
+betterNumber = favoriteNumber.to_i + 1
+puts 'Well, don\'t you think ' + betterNumber.to_s + ' would be a better number? It\'s a bigger, better version of your favorite number!'
---
+++ week1/ch05/full_name_greeting.rb
@@ -0,0 +1,7 @@
+puts 'Hello! What\'s your first name?'
+firstName= gets.chomp
+puts 'What a lovely name! I\'m sure your middle name is just as lovely. What\'s your middle name?'
+middleName = gets.chomp
+puts 'Awesome middle name! I wish I had your middle name. Can you tell me your last name too?'
+lastName = gets.chomp
+puts 'Well it\'s nice to meet you, ' +firstName + ' ' +middleName + ' ' +lastName + '! I\'m sure we are going to be great friends!'

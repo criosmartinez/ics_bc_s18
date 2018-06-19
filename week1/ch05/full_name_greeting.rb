6ac53dc6cbe49c6a273d572d5671cb40cd184957
criosmartinez <criosmartinez@berkeley.edu>
2018-06-19 12:12:10 -0700

    Changes requested by TA for Week1

--- week1/ch05/full_name_greeting.rb
+++ week1/ch05/full_name_greeting.rb
@@ -4,4 +4,4 @@ puts 'What a lovely name! I\'m sure your middle name is just as lovely. What\'s 
 middleName = gets.chomp
 puts 'Awesome middle name! I wish I had your middle name. Can you tell me your last name too?'
 lastName = gets.chomp
-puts 'Well it\'s nice to meet you, ' +firstName + ' ' +middleName + ' ' +lastName + '! I\'m sure we are going to be great friends!'
+puts 'Well it\'s nice to meet you, ' + firstName + ' ' + middleName + ' ' + lastName + '! I\'m sure we are going to be great friends!'

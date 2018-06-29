349b2616796f74a744a0d4b67df5e1c09c48c603
criosmartinez <criosmartinez@berkeley.edu>
2018-06-29 12:23:19 -0700

    Week 2 Revised

--- week2/ch07/99_bob.rb
+++ week2/ch07/99_bob.rb
@@ -1,10 +1,10 @@
 number = 99
-while 1<number
+while 1 < number
     puts number.to_s + ' bottles of beer on the wall, ' + number.to_s + ' bottles of beer.'
     number = number - 1
     puts 'Take one down and pass it around, ' + number.to_s + ' bottles of beer on the wall.'
     puts ' '
-    if number ==1
+    if number == 1
       puts number.to_s + ' bottle of beer on the wall, ' + number.to_s + ' bottle of beer.'
       puts 'Take one down and pass it around, no more bottles of beer on the wall.'
       puts ' '
--- week2/ch08/table_of_contents.rb
+++ week2/ch08/table_of_contents.rb
@@ -1,14 +1,23 @@
 line_width = 80
 
-#My Array
-table_of_contents = ['Harry Potter and the Sorcerer\'s Stone', 'Table of Contents', 'Chapter 1: The Boy Who Lived', '9', 'Chapter 2: The Vanishing Glass', '10', 'Chapter 3: The Letters from No One', '11', 'Chapter 4: The Keeper of the Keys', '20', 'Chapter 5: Diagon Alley', '200']
+#table_of_contents (old) = ['Harry Potter and the Sorcerer\'s Stone', 'Table of Contents', 'Chapter 1: The Boy Who Lived', '9', 'Chapter 2: The Vanishing Glass', '10', 'Chapter 3: The Letters from No One', '11', 'Chapter 4: The Keeper of the Keys', '20', 'Chapter 5: Diagon Alley', '200']
+
+#revised
+                      #title                                  , #page
+table_of_contents = [['Harry Potter and the Sorcerer\'s Stone'],
+                    ['Table of Contents'],
+                    ['Chapter 1: The Boy Who Lived', '9'],
+                    ['Chapter 2: The Vanishing Glass', '10'],
+                    ['Chapter 3: The Letters from No One', '11'],
+                    ['Chapter 4: The Keeper of the Keys', '20'],
+                    ['Chapter 5: Diagon Alley', '200']]
 
 #Printing my Array
 puts ' '
-puts table_of_contents[0].center(line_width)
-puts table_of_contents[1].center(line_width)
-puts table_of_contents[2].ljust(line_width-10) + 'page'.rjust(5) + table_of_contents[3].rjust(line_width/16)
-puts table_of_contents[4].ljust(line_width-10) + 'page'.rjust(5) + table_of_contents[5].rjust(line_width/16)
-puts table_of_contents[6].ljust(line_width-10) + 'page'.rjust(5) + table_of_contents[7].rjust(line_width/16)
-puts table_of_contents[8].ljust(line_width-10) + 'page'.rjust(5) + table_of_contents[9].rjust(line_width/16)
-puts table_of_contents[10].ljust(line_width-10) + 'page'.rjust(5) + table_of_contents[11].rjust(line_width/16)
+for title in 0..1
+  puts table_of_contents[title][0].center(line_width)
+end
+
+for chapter in 2..6
+  puts table_of_contents[chapter][0].ljust(line_width-10) + 'page'.rjust(5) + table_of_contents[chapter][1].rjust(line_width/16)
+end
